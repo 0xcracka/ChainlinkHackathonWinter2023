a382d6d7dced72ff3af943588e0154aeaa355bdd
abasedape <Skylor808@gmail.com>
2023-11-29 10:10:09 -1000

    added api key

--- application/pages/_app.tsx
+++ application/pages/_app.tsx
@@ -22,10 +22,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           // Set active chain for app
           activeChain={CHAIN}
           // Auth (SIWE) configuration
-          authConfig={{
-            domain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || "evmkit.com", // Your website domain
-            authUrl: "/api/auth", // API Route (default is - pages/api/auth/[...thirdweb].ts)
-          }}
+          // authConfig={{
+          //   domain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || "evmkit.com", // Your website domain
+          //   authUrl: "/api/auth", // API Route (default is - pages/api/auth/[...thirdweb].ts)
+          // }}
           clientId={process.env.NEXT_PUBLIC_THIRDWEB_API_KEY}
         >
           <Nav />
--- application/pages/api/validateDataset.js
+++ application/pages/api/validateDataset.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
-  res.status(200).json({ validated: true });
+  // Check for the correct HTTP method if necessary
+  if (req.method === "GET") {
+    // Return the expected JSON structure
+    res.status(200).json({ isValidated: true });
+  } else {
+    // Handle other HTTP methods or send an error
+    res.status(405).send("Method Not Allowed");
+  }
 }
