c9fe1a2ad2e2d8d59b5ee69113f79ce9734e7486
abasedape <Skylor808@gmail.com>
2023-11-29 10:17:09 -1000

    commented out nftgrid for now

--- application/pages/marketplace.tsx
+++ application/pages/marketplace.tsx
@@ -9,13 +9,13 @@ const marketplace = () => {
         Browse which NFTs are available from the collection.
       </p>
       <div className="max-w-3xl mx-auto mt-8 p-4 bg-white rounded-lg shadow">
-        <NFTGrid1
+        {/* <NFTGrid1
           // data={data}
           // isLoading={isLoading}
           emptyText={
             "Looks like there are no NFTs in this collection. Did you import your contract on the thirdweb dashboard? https://thirdweb.com/dashboard"
           }
-        />
+        /> */}
       </div>
     </div>
   );
